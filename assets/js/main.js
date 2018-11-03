cb495933c90b5f81a865217b3a69e8ed124ae380
ewotawa <ewotawa@users.noreply.github.com>
2018-11-03 22:17:49 +0000

    deprecate jQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,65 @@
-/* Function to keep nav bar at top of window */
-function adjustTop() {
-        var height = $('.main-nav').height();
-        $('.main-wrapper').css({'top' : height + 'px'});
-}    
-
 /* smooth scroll */
 
-$(document).on('click', 'a', function(event){
-    event.preventDefault();
-
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
+document.addEventListener('click', function(event) {
+    let targetId = event.target.id;
+    console.log(targetId);
+    targetId.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 });
+
+/* call resizeIframes function */
+
+resizeIframes();
+
+/* https://css-tricks.com/snippets/jquery/fit-iframe-to-content/ */
+/* update to deprecate jquery */
+
+    function resizeIframes() {
+    
+        var iFrames = document.getElementsByTagName('iframe');
+
+    	function iResize() {
+    	
+    		for (var i = 0, j = iFrames.length; i < j; i++) {
+    		  iFrames[i].style.height = iFrames[i].contentWindow.document.body.offsetHeight + 'px';}
+    	    }
+    	    
+        	// handler for safari and opera browsers
+        	
+        	let sUsrAg = window.navigator.userAgent;
+        	
+        	if (sUsrAg.indexOf("Safari") > -1 || sUsrAg.indexOf("Opera") > -1) { 
+        	
+        	   iFrames.load(function(){
+        	       setTimeout(iResize, 0);
+               });
+            
+        	   for (var i = 0, j = iFrames.length; i < j; i++) {
+        			var iSource = iFrames[i].src;
+        			iFrames[i].src = '';
+        			iFrames[i].src = iSource;
+               }
+               
+        	} else {
+        	   iFrames.load(function() { 
+        	       this.style.height = this.contentWindow.document.body.offsetHeight + 'px';
+        	   });
+        	}
+        
+        }
+
+
+
+
+
+
+
+
+
+
 
 /* https://css-tricks.com/snippets/jquery/fit-iframe-to-content/ */
 
+/*
     $(function(){
     
         var iFrames = $('iframe');
@@ -46,3 +90,4 @@ $(document).on('click', 'a', function(event){
         
         });
 
+*/
