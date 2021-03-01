cf5043f7dd053783c86c507389c5622b9a0ea70a
Elizabeth Wotawa <ewotawa@users.noreply.github.com>
2021-03-01 01:26:31 -0600

    remove service worker from project, read JSON for portfolio successfully

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
+/* render elements to the DOM from JSON files */
+
+/* PORTFOLIO */
+
+/* load JSON data into a variable */
+const portfolioStr = JSON.stringify(portfolio);
+const portfolioParsed = JSON.parse(portfolioStr);
+
+console.log(portfolioParsed[0].title);
+console.log(portfolioParsed[0].path);
 
 /* smooth scroll */
 
@@ -16,38 +26,35 @@ resizeIframes();
 /* https://css-tricks.com/snippets/jquery/fit-iframe-to-content/ */
 
     function resizeIframes() {
-    
+
         var iFrames = document.getElementsByTagName('iframe');
 
     	function iResize() {
-    	
+
     		for (var i = 0, j = iFrames.length; i < j; i++) {
     		  iFrames[i].style.height = iFrames[i].contentWindow.document.body.offsetHeight + 'px';}
     	    }
-    	    
+
         	// handler for safari and opera browsers
-        	
+
         	let sUsrAg = window.navigator.userAgent;
-        	
-        	if (sUsrAg.indexOf("Safari") > -1 || sUsrAg.indexOf("Opera") > -1) { 
-        	
-        	   iFrames.load(function() { 
+
+        	if (sUsrAg.indexOf("Safari") > -1 || sUsrAg.indexOf("Opera") > -1) {
+
+        	   iFrames.load(function() {
         	       setTimeout(iResize, 0);
                });
-            
+
         	   for (var i = 0, j = iFrames.length; i < j; i++) {
         			var iSource = iFrames[i].src;
         			iFrames[i].src = '';
         			iFrames[i].src = iSource;
                }
-               
+
         	} else {
-        	   iFrames.load(function() { 
+        	   iFrames.load(function() {
         	       this.style.height = this.contentWindow.document.body.offsetHeight + 'px';
         	   });
         	}
-        
+
         }
-
-
-
